d551ea221083ab7ace5943257ed1702160a2664d
Wendell <vvendell0304@gmail.com>
2022-08-23 10:07:27 +0800

    replace portfolio images for polaris project

--- js/slideshow.js
+++ js/slideshow.js
@@ -7,7 +7,11 @@ let images = [
     "./assets/images/1.png",
     "./assets/images/2.png",
     "./assets/images/3.png",
-    "./assets/images/4.png"
+    "./assets/images/4.png",
+    "./assets/images/5.png",
+    "./assets/images/6.png",
+    "./assets/images/7.png",
+    "./assets/images/8.png"
 ];
 
 let time = 1000;
