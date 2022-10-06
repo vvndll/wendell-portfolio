3c5aee084325ccc1d96e73629747db6c25e9dca7
Wendell <vvendell0304@gmail.com>
2022-10-06 17:03:38 +0800

    updated projects

--- js/slideshow.js
+++ js/slideshow.js
@@ -4,14 +4,15 @@ const slide = document.querySelector("div.preview__box img");
 
 let counter = 0;
 let images = [
-    "./assets/images/1.png",
-    "./assets/images/2.png",
-    "./assets/images/3.png",
-    "./assets/images/4.png",
-    "./assets/images/5.png",
+    "./assets/images/1.jpg",
+    "./assets/images/2.jpg",
+    "./assets/images/3.jpg",
+    "./assets/images/4.jpg",
+    "./assets/images/5.jpg",
     "./assets/images/6.png",
     "./assets/images/7.png",
-    "./assets/images/8.png"
+    "./assets/images/8.png",
+    "./assets/images/9.png"
 ];
 
 let time = 1000;
