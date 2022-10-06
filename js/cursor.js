348442b995ca72e0f5dc12181002ee82f17084a4
Wendell <vvendell0304@gmail.com>
2022-10-06 17:11:55 +0800

    show cursor

--- js/cursor.js
+++ js/cursor.js
@@ -1,28 +1,28 @@
-'use strict';
+// 'use strict';
 
 
-//For the Cursor---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-const cursorR = document.querySelector(".cursor__right");
-const cursorB = document.querySelector(".cursor__bottom");
-const cursorL = document.querySelector(".cursor__left");
-const cursorT = document.querySelector(".cursor__top");
+// //For the Cursor---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// const cursorR = document.querySelector(".cursor__right");
+// const cursorB = document.querySelector(".cursor__bottom");
+// const cursorL = document.querySelector(".cursor__left");
+// const cursorT = document.querySelector(".cursor__top");
 
-document.addEventListener("mousemove", moveCursor);
+// document.addEventListener("mousemove", moveCursor);
 
-function moveCursor(e) {
-    let x = e.clientX;
-    let y = e.clientY;
+// function moveCursor(e) {
+//     let x = e.clientX;
+//     let y = e.clientY;
 
 
-    cursorR.style.left = `${x}px`;
-    cursorR.style.top = `${y}px`;
-    cursorB.style.left = `${x}px`;
-    cursorB.style.top = `${y}px`;
-    cursorL.style.left = `${x}px`;
-    cursorL.style.top = `${y}px`;
-    cursorT.style.left = `${x}px`;
-    cursorT.style.top = `${y}px`;
-};
+//     cursorR.style.left = `${x}px`;
+//     cursorR.style.top = `${y}px`;
+//     cursorB.style.left = `${x}px`;
+//     cursorB.style.top = `${y}px`;
+//     cursorL.style.left = `${x}px`;
+//     cursorL.style.top = `${y}px`;
+//     cursorT.style.left = `${x}px`;
+//     cursorT.style.top = `${y}px`;
+// };
 
 
 
